6a024e2c88d673319303594e467b58e52fe9623e
marcos-sco <marcos_sco@outlook.com>
2025-06-28 00:13:03 -0300

    fix: env process

--- todo-frontend/vite.config.js
+++ todo-frontend/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
-  const baseUrl = env.VITE_BASE_URL ?? '/';
+  const baseUrl = env.VITE_BASE_URL || '/';
 
   return {
     base: baseUrl,
