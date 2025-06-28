7f7fd5f50c0e50490682c99c664da1c28ab0866c
marcos-sco <marcos_sco@outlook.com>
2025-06-27 22:02:45 -0300

    build: change configs

--- todo-frontend/vite.config.js
+++ todo-frontend/vite.config.js
@@ -1,26 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite'
 
-const isLocalhost = process.env.NODE_ENV === 'development';
-
 // https://vite.dev/config/
-export default defineConfig({
-  // base: isLocalhost ? '/' : '/todo-before-test/',
-  plugins: [
-    vue(),
-    tailwindcss()
-  ],
-  server: {
-    host: true, // Allow access from the network
-    port: 5173, // Use the port exposed in Docker app service
-    strictPort: true, // Fail if the port is already in use
-    hmr: {
-      host: 'localhost',
-      protocol: 'ws',
-    },
-    watch: {
-      usePolling: true, // Enable polling for file changes
-    },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const baseUrl = env.VITE_BASE_URL ?? '/';
+
+  return {
+    base: baseUrl,
+    plugins: [
+      vue(),
+      tailwindcss()
+    ],
+    server: {
+      host: true,
+      port: 5173,
+      strictPort: true,
+      hmr: {
+        host: 'localhost',
+        protocol: 'ws',
+      },
+      watch: {
+        usePolling: true,
+      },
+    }
   }
-})
+});
